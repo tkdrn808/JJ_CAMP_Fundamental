d6f6387f63ca6e093f06628f38979b32708edc79
Jee Hoon <yamoo9@naver.com>
2016-10-01 11:24:58 +0900

    자바스크립트 실행 흐름 제어

---
+++ LECTURE/DAY02/core/dom-control.js
@@ -0,0 +1,32 @@
+// [Design, 설계] 참조하고자 하는 문서 객체에 적절한 메모리 이름 공간을 변수 이름으로 설정한다.
+// var html          = document.documentElement,
+//     body          = document.body,
+//     page          = document.getElementById('page'),
+//     page_headline = page.getElementsByTagName('h1'),
+//     page_child    = page.getElementsByClassName('page-child');
+
+// 코드가 실행되는 시점(Event)을 감지하여 코드를 처리한다.
+// 이벤트 프로그래밍
+
+// window 객체의 이벤트 속성(Event Property)
+console.log( 'window.onload:', window.onload ); // null [초기 값]
+
+// var action = function() { console.log('ready action!'); };
+
+// 초기화 수행
+var initialization = function() {
+  var html          = document.documentElement,
+      body          = document.body,
+      page          = document.getElementById('page'),
+      page_headline = page.getElementsByTagName('h1'),
+      page_child    = page.getElementsByClassName('page-child');
+
+  console.log('html:', html);
+  console.log('body:', body);
+  console.log('page:', page);
+  console.log('page_headline:', page_headline);
+  console.log('page_child:', page_child);
+};
+
+// window.onload = action;
+window.onload = initialization;
