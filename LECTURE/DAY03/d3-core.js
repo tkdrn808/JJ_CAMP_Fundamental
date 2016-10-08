0ef42873f46d8f0691f592cfb21733e1e250f11f
Jee Hoon <yamoo9@naver.com>
2016-10-08 15:31:54 +0900

    데이터 유형을 체크하는 방법 typeof instanceof

--- LECTURE/DAY03/d3-core.js
+++ LECTURE/DAY03/d3-core.js
@@ -134,6 +134,130 @@ var navigation_item_04 = 'Products';
 var navigation_item_05 = 'Contact';
 
 // 배열을 사용할 때 연관된 복수 데이터 관리 (쉽고 효율)
-var navigation_items = new Array('Home', 'About', 'Works', 'Products', 'Contact');
+// var navigation_items = new Array('Home', 'About', 'Works', 'Products', 'Contact');
+var navigation_items = [];
+
+navigation_items.push('Home');
+navigation_items.push('About');
+navigation_items.push('Works');
+navigation_items.push('Products');
+navigation_items.push('Contact');
 
 console.log( 'navigation_items:', navigation_items );
+console.log( 'navigation_items.length:', navigation_items.length );
+
+// 아래와 같은 방법은 문제를 수반하기에 권장하고 싶지 않다.
+// navigation_items[5] = 'board';
+// navigation_items[6] = 'favorites';
+
+// 배열 객체의 메소드 .push() 사용하길 권장!!!!
+navigation_items.push('board');
+navigation_items.push('favorites');
+
+console.log( 'navigation_items:', navigation_items );
+console.log( 'navigation_items.length:', navigation_items.length );
+
+
+// 연관배열(문자 유형의 속성을 배열 객체에 추가)
+
+var drinks = [];
+
+drinks.push('milk');
+drinks.push('juice');
+drinks.push('soju');
+
+console.log( drinks.length ); // 3
+
+drinks['clear'] = '음료를 비우다';
+
+console.dir(drinks);
+
+
+// ------------------------------------
+// 객체의 속성에 접근하는 표기법
+
+// 1. 점 기호를 통한 접근
+console.log( drinks.clear );
+
+// 2. 각괄호를 사용한 접근
+// (각괄호 내부에 문자열을 넣어줘야 함)
+// 일반적으로 자주 사용되지는 않지만,
+// 반복문(Loop) 내부에서 특정 변수 값을
+// 순환할 때 종종 사용된다.
+console.log( drinks['clear'] );
+
+// 자바스크립트에 내장된 객체들
+// 숫자 객체
+// 문자 객체
+// 불리언 객체
+// 함수 객체
+// 배열 객체
+// 일반 객체
+
+// 위 객체를 생성하는 구문보다는 리터럴을 바로 사용하는 것이 좋다.
+var num, str, boo, fnc, arr, obj;
+
+num = 10;
+str = 'java vs javascript';
+boo = !false;
+fnc = function() {};
+arr = [];
+obj = {};
+
+// CSS 문법과 유사해 보이는 객체 리터럴 표현식
+var css_props = {
+  'font-size': '24px',
+  'color': '#34129f',
+  'border-top-color': 'currentColor'
+};
+
+// 결과는 자바스크립트 객체이다.
+// Object {font-size: "24px", color: "#34129f", border-top-color: "currentColor"}
+
+// delete 키워드를 사용하면 객체의 속성을 제거할 수 있다.
+// 단, 전역(Global Scope) 변수는 지울 수 없다.
+delete css_props.color; // color 속성이 제거된다.
+delete css_props; // css_props 는 제거되지 않는다. css_props는 전역 변수이니까.
+
+// 자바스크립트 자료형(Data Type)
+
+// 숫자형
+// 문자형
+// 불리언형
+// 객체형
+// 배열형
+// 함수형
+// null
+// undefined
+
+/**
+ * --------------------------------
+ * 올바른 데이터 유형을 체크하는 방법
+ * ----------------------------- */
+
+// 방법 1. typeof 키워드 (함수가 아님)
+console.log('typeof num:', typeof num);             // 'number'
+console.log('typeof str:', typeof str);             // 'string'
+console.log('typeof boo:', typeof boo);             // 'boolean'
+console.log('typeof fnc:', typeof fnc);             // 'function'
+console.log('typeof arr:', typeof arr);             // 'array'
+console.log('typeof obj:', typeof obj);             // 'object'
+console.log('typeof undefined:', typeof undefined); // 'undefined'
+console.log('typeof null:', typeof null);           // 'null'
+
+// ※ typeof 키워드 사용 시 주의할 점!
+// null, Array를 올바르게 인식하지 못한다.
+// 둘 다 모두 'object'를 반환한다.
+
+// 설계 도면(class, 생성자(Constructor))
+// 조각(실체화된 객체) instance
+
+var playlist = new Array();
+
+console.log('typeof playlist:', typeof playlist); // object
+console.log( playlist instanceof Array ); // true
+
+// Sketch 데모의 사례를 코드로 구현하자면....
+// var page1_ins = new CircleHack();
+// var page2_ins = new CircleHack();
+// var page3_ins = new CircleHack();
