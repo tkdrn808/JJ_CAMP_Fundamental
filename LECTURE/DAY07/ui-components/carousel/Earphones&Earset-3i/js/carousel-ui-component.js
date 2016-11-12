57a4ccfaa3ab4c4e507092305b5b93c4c7712796
Jee Hoon <yamoo9@naver.com>
2016-11-12 12:58:20 +0900

    HTML 문서에 추가했던 스타일 관련 문구를 JS로 옮김

--- LECTURE/DAY07/ui-components/carousel/Earphones&Earset-3i/js/carousel-ui-component.js
+++ LECTURE/DAY07/ui-components/carousel/Earphones&Earset-3i/js/carousel-ui-component.js
@@ -5,6 +5,7 @@
 // 고로 이벤트가 필요.
 window.onload = init;
 
+// --------------------------------------------------------------------------------
 // 캐러셀 컴포넌트 초기화(Init, Initialization)
 function init() {
 
@@ -32,13 +33,31 @@ function init() {
   // .carousel-view 영역의 가로 폭을 포함하는 이미지 개수의 폭의 합 만큼 설정
   view.style.width = view_width + 'px';
 
+  // 요소.clientWidth 속성
+  // 소수점을 허용하지 않고, 값을 올림하여 정수를 반환한다.
+  // content-box + padding-box 까지의 폭을 반환한다.
+
+  // 요소.style.width 속성
+  // 인라인 스타일 속성이 설정된 요소의 width 너비를 가져온다.
+  // 인라인 스타일 속성이 설정되어 있지 않을 경우는 빈 문자열 ''을 반환한다.
+
+  // window.getComputedStyle(요소).width
+  // 웹 브라우저가 최종적으로 계산한 width 값을 반환한다.
+  // 다만, 이 방법은 표준 방법이지만 표준을 준수하지 않은 IE 8- 브라우저에서는
+  // 작동하지 않는다.
+
+  // 요소.currentStyle.width
+  // 비 표준 방법으로 Microsoft Internet Explorer 에서만 동작한다.
+  // 가져오는 값은 .getComputedStyle(요소).width과 같다.
+
   // 각 이미지를 순환 처리하여 컨테이너 요소의 너비 만큼 이미지 너비를 설정한다.
   for (var i=0; i<view_contents_length; i++ ) {
-    var view_content = view_contents.item(i);
+    var view_content = view_contents.item(i); // 0
     // console.log(view_content); // 각 <img> 출력
     view_content.style.width = view_content_width + 'px';
   }
 
+  // --------------------------------------------------------------------------------
   // 버튼 이벤트 연결
   // 버튼의 부모 요소에게 이벤트 위임
   var prev_btn = controls.querySelector('.carousel-previous-btn');
@@ -58,8 +77,13 @@ function init() {
   function nextMoveCarouselContentView(){
     active_index = ++active_index % view_contents_length;
     view.style.transform = 'translateX('+ (-1 * view_content_width * active_index) +'px)';
+    // view.style.position = 'absolute';
+    // view.style.top = '0px';
+    // view.style.left = -1 * view_content_width * active_index +'px';
   }
 
+  // --------------------------------------------------------------------------------
+  // 인디케이터 (표시자)
   var indicator_items = controls.querySelectorAll('.carousel-tab');
 
 }
