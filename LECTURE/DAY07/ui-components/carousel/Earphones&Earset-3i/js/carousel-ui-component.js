2111f8c81d59985478bb718ed5c7f7a9eb5ffcce
Jee Hoon <yamoo9@naver.com>
2016-11-12 14:28:34 +0900

    매개변수 보충

--- LECTURE/DAY07/ui-components/carousel/Earphones&Earset-3i/js/carousel-ui-component.js
+++ LECTURE/DAY07/ui-components/carousel/Earphones&Earset-3i/js/carousel-ui-component.js
@@ -17,11 +17,15 @@ localScope();
 console.log(3, mouse);
 // 매개 변수
 // parameter
-function callMe(who) {
+function callMe(who, when, where, how) {
   // 매개변수는 해당 함수의 내부에서 접근이 가능하다.
-  console.log('a', who);
+  // console.log('a', who);
+  console.log('who:', who);
+  console.log('when:', when);
+  console.log('where:', where);
+  console.log('how:', how);
 }
-callMe('boy');
+callMe('boy', 'yesterday', 'school', 'using smartphone');
 // console.log('b', who); // Reference Error
 
 // 멤버 변수
