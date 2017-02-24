72348846f650f554d9e9fb3b858a2ac43cac4fa4
Jee Hoon <yamoo9@naver.com>
2017-02-24 23:48:50 +0900

    기존 수업 자료 제거

--- __/DAY01/js/write_html_code.js
+++
@@ -1,4 +0,0 @@
-document.write('<h1>Language of World Wide Web</h1>');
-document.write('<p>'+ markup_language +'은 '+ markup_language_eval +'.</p>');
-document.write('<p>'+ style_language +'는 '+ style_language_eval +'.</p>');
-document.write('<p>'+ behavior_language +'는 '+ behavior_language_eval +'.</p>');
